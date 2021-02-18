03d105b5963661b64c4254f8b5db24da2caa1c5e
Justine <justinemalphettes@hotmail.fr>
2021-02-18 10:13:08 +0100

    dernière version

--- src/Modèle.java
+++ src/Modèle.java
@@ -1,10 +1,31 @@
 import java.awt.Color;
+import java.util.Random;
 
 public class Modèle {
-	Color[] couleurs;
-	int n_tentatives=10;
+	static Color[] couleurs = {Color.yellow, Color.green, Color.blue, Color.magenta, Color.red, Color.orange,Color.white,Color.black};
+	static int n_tentatives=10;
 	static int difficulté=4;
-	enum états {EN_COURS,GAGNE,PERDU};
+	enum Etats {EN_COURS,GAGNE,PERDU};
+	Rangée[] propositions;
+	Etats état;
+	int tentative;
+	Rangée combinaison;
 	
+	
+	public Modèle() {
+		this.état=Etats.EN_COURS;
+		this.tentative=0;
+		this.propositions=new Rangée[n_tentatives];
+		this.propositions[this.tentative]=new Rangée();
+		this.combinaison=new Rangée();
+		Random random=new Random();
+		int i;
+		for (i=0; i<this.difficulté;i++) {
+			int nb=random.nextInt(couleurs.length);
+			this.combinaison.jetons[i]=couleurs[nb];
+		}
+		
+		
+	}
 
 }
--- src/Rangée.java
+++ src/Rangée.java
@@ -3,8 +3,14 @@ import java.awt.Color;
 public class Rangée {
 
 	Color [] jetons;
-	int taille=Modèle.difficulté;
+	int indiceJeton;
+	int nbnoirs,nbblancs;
 	
-	
+	public Rangée () {
+		this.jetons = new Color[Modèle.difficulté];
+		this.indiceJeton=0;
+		this.nbnoirs=0;
+		this.nbblancs=0;
+	}
 	
 }
