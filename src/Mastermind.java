3f4b04361fac24d7f6929d7e983daf4ba4517d32
Oceane31 <78899865+Oceane31@users.noreply.github.com>
2021-02-18 08:50:14 +0100

    Ajout du mastermind

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,8 +1,12 @@
 
 public class Mastermind {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Modèle m=new Modèle();
+		Contrôleur c = new Contrôleur(m);
+		Vue v= new Vue(m,c);
 
 	}
 
